c6aa1f85b3f17284ef4fc74243fbd3bfeb7796ce
Kirill Mangutov <kmangutov@gmail.com>
2021-01-16 12:14:56 -0800

    Add spot and future feed, writes to sqlite, start dockerfile

---
+++ src/feed/db_access.py
@@ -0,0 +1,67 @@
+import aiosqlite
+import datetime
+import asyncio
+
+
+# Scale sqlite: https://stackoverflow.com/questions/784173/what-are-the-performance-characteristics-of-sqlite-with-very-large-database-file
+SQL_CREATE_CANDLES = """CREATE TABLE IF NOT EXISTS Candles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    timestamp_sql TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    timestamp_exchange INTEGER,
+    exchange VARCHAR(16),
+    symbol VARCHAR(16),
+    asset_type VARCHAR(8),
+    open FLOAT,
+    high FLOAT,
+    low FLOAT,
+    close FLOAT,
+    volume FLOAT
+)"""
+
+
+SQL_INSERT_CANDLE = """
+INSERT INTO Candles(timestamp_exchange, exchange, symbol, asset_type, open, high, low, close, volume)
+VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+"""
+
+
+async def build_table_candles(db):
+    cursor = await db.execute(SQL_CREATE_CANDLES)
+    await cursor.close()
+
+async def insert_candle(db,
+                        exchange,
+                        symbol,
+                        asset_type,
+                        candle):
+    candle_tuple = (
+        candle[0][0],
+        exchange,
+        symbol,
+        asset_type,
+        candle[0][1],
+        candle[0][2],
+        candle[0][3],
+        candle[0][4],
+        candle[0][5]
+    )
+
+    print(SQL_INSERT_CANDLE)
+    print(candle_tuple)
+
+    cursor = await db.execute(SQL_INSERT_CANDLE, candle_tuple)
+    await db.commit()
+    await cursor.close()
+    
+
+async def connect():
+    db = await aiosqlite.connect('./data/ts.db')
+    await build_table_candles(db)
+    return db
+
+
+async def main():
+    db = await connect()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
---
+++ src/feed/spot_future_feed.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import db_access
+import asyncio
+import time
+import datetime
+
+USE_PRO = False
+if USE_PRO:
+    import ccxtpro
+else:
+    import ccxt as ccxtpro
+
+
+STRING_EXCHANGE = 'binance'
+
+# Multiple exchanges: https://github.com/ccxt/ccxt/blob/master/examples/py/async-orderbooks-from-multiple-exchanges-at-once.py
+# Spot vs future: https://github.com/ccxt/ccxt/blob/master/examples/py/async-binance-futures-vs-spot.py
+    
+async def loop(db, exchange, symbol, asset_type):
+    while True:
+        try:
+            exchange.options['defaultType'] = asset_type
+            
+            candle = []
+            if USE_PRO:
+                candle = await exchange.watch_ohlcv(symbol)
+            else:
+                time.sleep(exchange.rateLimit / 1000) # time.sleep wants seconds
+                since = datetime.datetime.now() - datetime.timedelta(minutes=1)
+                candle = exchange.fetch_ohlcv(symbol, timeframe='1m', since=since.timestamp())
+                candle = [candle[len(candle) - 1]] # Put in array so its backwards compatible with watch_ohlcv
+
+            now = exchange.milliseconds()
+            print(exchange.iso8601(now), STRING_EXCHANGE, symbol, asset_type, symbol, candle)#orderbook['asks'][0], orderbook['bids'][0])
+            await db_access.insert_candle(db, STRING_EXCHANGE, symbol, asset_type, candle)
+        except Exception as e:
+            print(str(e))
+            # raise e  # uncomment to break all loops in case of an error in any one of them
+            # break  # you can also break just this one loop if it fails
+
+
+# TODO: USE_PRO=True works but for USE_PRO=False CCXT, need separate spot and future instanes
+# with exchange.options['defaultType'] = asset_type.
+async def main():
+    db = await db_access.connect()
+
+    exchange = ccxtpro.binance({'enableRateLimit': True})
+
+    symbols = ['BTC/USDT']
+    asset_types = ['spot', 'future']
+    await asyncio.gather(*[loop(db, exchange, symbol, asset_type) for symbol in symbols for asset_type in asset_types])
+    await exchange.close()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
